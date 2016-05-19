e9f5f8097ac011d9a2882b440169ea2d6b9fef12
victor-hecs <herseed@163.com>
2016-05-19 16:44:39 +0800

    Update bme280_example.ino
    alter comments to make it clear

--- example/bme280_example/bme280_example.ino
+++ example/bme280_example/bme280_example.ino
@@ -44,19 +44,23 @@ void setup()
 void loop()
 {
   float pressure;
-
+  
+  //get and print temperatures
   Serial.print("Temp: ");
   Serial.print(bme280.getTemperature());
-  Serial.println("C");
-
+  Serial.println("C");//The unit for  Celsius because original arduino don't support speical symbols
+  
+  //get and print atmospheric pressure data
   Serial.print("Pressure: ");
   Serial.print(pressure = bme280.getPressure());
   Serial.println("Pa");
 
+  //get and print altitude data
   Serial.print("Altitude: ");
   Serial.print(bme280.calcAltitude(pressure));
   Serial.println("m");
 
+  //get and print humidity data
   Serial.print("Humidity: ");
   Serial.print(bme280.getHumidity());
   Serial.println("%");
